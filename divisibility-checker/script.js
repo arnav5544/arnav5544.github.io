374345b73e2d7ce7080f27eee18e546b1f3168d8
Fact 2 You <60618162+arnav5544@users.noreply.github.com>
2020-08-08 16:29:14 +0530

    Create script.js

---
+++ divisibility-checker/script.js
@@ -0,0 +1,29 @@
+//made by arnav kumar
+//copyright all rights reserved
+//writing function to check divisibility
+function check(first,second){
+if(first%second==0)
+{
+    alert(first+" is divisible by "+second);
+    
+    
+}
+else{
+        alert(first+" is not divisible by "+second)
+    }
+    //giving an alert box that will show the result on dividing
+    alert("on dividing, "+first+" by "+second+", the result was "+first/second);
+//function is done now we will test it
+}
+//its fully working 
+
+
+function check_divisibility(){
+    var first= Number(document.getElementById("first").value)
+var second=Number(document.getElementById("second").value)
+check(first,second)
+}
+
+
+
+
