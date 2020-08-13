bd1640de3a777e1a668f221ddddaabc29b42c78f
Fact 2 You <60618162+arnav5544@users.noreply.github.com>
2020-08-13 17:54:04 +0530

    Update script.js

--- meet/mobile/script.js
+++ meet/mobile/script.js
@@ -1,5 +1,5 @@
 // Generate random room name if needed
-var meetId=Math.floor(Math.random() * 0xFFFFFF).toString(16);
+
 if (window.location=="https://arnav5544.github.io/meet/mobile" && !location.hash) {
   location.hash =localStorage.meetInput
 }
