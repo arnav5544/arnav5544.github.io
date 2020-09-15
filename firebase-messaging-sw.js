cab36985838eedf1830ac327767729d7e61c8942
Fact 2 You <60618162+arnav5544@users.noreply.github.com>
2020-09-15 16:42:36 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -32,14 +32,14 @@ const messaging = firebase.messaging();
 messaging.setBackgroundMessageHandler(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
-  const notificationTitle = 'Background Message Title';
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: 'Background Message body.',
+    body: payload.notification.body,
     icon: '/arnav.png',
     badge:'/arnav.png'
   };
 
-  return self.registration.showNotification(payload.data.title,
+  return self.registration.showNotification(notificationTitle,
     notificationOptions);
 });
 // [END background_handler]
