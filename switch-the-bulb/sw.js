3672ceb7e35b9d74734f1f49c769196cf61ac139
Fact 2 You <60618162+arnav5544@users.noreply.github.com>
2020-09-20 21:39:29 +0530

    Create sw.js

---
+++ switch-the-bulb/sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('arnav5544').then(function(cache) {
+     return cache.addAll([
+       'https://arnav5544.github.io/switch-the-bulb',
+       'index.html',
+       'script.js',
+       'bulb.png',
+'arnav.png',
+'switch-the-bulb.webmanifest',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
