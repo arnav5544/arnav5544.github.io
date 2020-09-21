1ba1d929632fbc7240a9a3f1c369362031e2a246
Fact 2 You <60618162+arnav5544@users.noreply.github.com>
2020-09-21 08:04:22 +0530

    Update script.js

--- switch-the-bulb/script.js
+++ switch-the-bulb/script.js
@@ -1,4 +1,4 @@
 // Register service worker to control making site work offline
 
- if('serviceWorker' in navigator) { navigator.serviceWorker .register('sw.js') .then(function() { console.log('Service Worker Registered'); }); } 
+ if('serviceWorker' in navigator) { navigator.serviceWorker .register('bulb-sw.js') .then(function() { console.log('Service Worker Registered'); }); } 
  
